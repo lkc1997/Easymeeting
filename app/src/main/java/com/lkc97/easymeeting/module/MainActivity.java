2951425c5eb4797d0dc45eef7e92f19db1c0c7a8
lkc1997 <kechenglu97@gmail.com>
2018-01-25 18:48:24 +0800

    初步配置

--- app/src/main/java/com/lkc97/easymeeting/module/MainActivity.java
+++ app/src/main/java/com/lkc97/easymeeting/module/MainActivity.java
@@ -1,7 +1,9 @@
-package com.lkc97.easymeeting;
+package com.lkc97.easymeeting.module;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lkc97.easymeeting.R;
 
 public class MainActivity extends AppCompatActivity {
 
