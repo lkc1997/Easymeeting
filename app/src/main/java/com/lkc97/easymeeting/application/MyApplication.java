13ec801279b3e1f561d228e7e7f842c5ee9ae6aa
lkc1997 <kechenglu97@gmail.com>
2018-01-28 17:52:04 +0800

    添加底部导航

---
+++ app/src/main/java/com/lkc97/easymeeting/application/MyApplication.java
@@ -0,0 +1,22 @@
+package com.lkc97.easymeeting.application;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVOSCloud;
+
+/**
+ * Created by 77416 on 2018/1/27.
+ */
+
+public class MyApplication extends Application {
+    private final String AppID="GlS5n1W7dxjFBEnxRu2GLIvl-gzGzoHsz";
+    private final String AppKey="49BNFGxJt0VqnB4l9KKXIHOP";
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        // 初始化LeacCloud的App ID 以及 App Key
+        AVOSCloud.initialize(this,AppID,AppKey);
+        // 开启调试日志(网络请求、错误消息等信息输出到 IDE 的日志窗口)
+        AVOSCloud.setDebugLogEnabled(true);
+    }
+}
---
+++ app/src/main/java/com/lkc97/easymeeting/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.lkc97.easymeeting.ui.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lkc97.easymeeting.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
