d2da855b9ea6822f3c6f20ae4c1f618f0831e024
lkc1997 <kechenglu97@gmail.com>
2018-02-11 22:57:51 +0800

    common

---
+++ app/src/main/java/com/lkc97/easymeeting/data/manager/RegisterManager.java
@@ -0,0 +1,8 @@
+package com.lkc97.easymeeting.data.manager;
+
+/**
+ * Created by 77416 on 2018/2/11.
+ */
+
+public class RegisterManager {
+}
---
+++ app/src/main/java/com/lkc97/easymeeting/data/network/Register.java
@@ -0,0 +1,8 @@
+package com.lkc97.easymeeting.data.network;
+
+/**
+ * Created by 77416 on 2018/2/11.
+ */
+
+public class Register {
+}
