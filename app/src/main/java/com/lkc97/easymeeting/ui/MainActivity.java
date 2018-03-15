edefb155940906219ee0db20d247e042904f0201
lkc1997 <kechenglu97@gmail.com>
2018-03-15 19:33:59 +0800

    完善好友列表

--- app/src/main/java/com/lkc97/easymeeting/ui/MainActivity.java
+++ app/src/main/java/com/lkc97/easymeeting/ui/MainActivity.java
@@ -14,45 +14,6 @@ import com.lkc97.easymeeting.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ConfFragment confFrag;
-
-    /*设置默认Fragment*/
-    private void setDefaultFrag() {
-        if (confFrag == null) {
-            confFrag = new ConfFragment();
-        }
-        addFrag(confFrag);
-        /*默认显示confFrag*/
-        getSupportFragmentManager().beginTransaction().show(confFrag).commit();
-    }
-
-    /*添加Frag*/
-    private void addFrag(Fragment frag) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        if (frag != null && !frag.isAdded()) {
-            ft.add(R.id.main_fragment, frag);
-        }
-        ft.commit();
-    }
-
-    /*隐藏所有fragment*/
-    private void hideAllFrag() {
-        hideFrag(confFrag);
-        //hideFrag(topicFrag);
-        //hideFrag(newconFrag);
-        //hideFrag(friendFrag);
-        //hideFrag(mineFrag);
-    }
-
-    /*隐藏frag*/
-    private void hideFrag(Fragment frag) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        if (frag != null && frag.isAdded()) {
-            ft.hide(frag);
-        }
-        ft.commit();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,53 +27,5 @@ public class MainActivity extends AppCompatActivity {
             loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(loginIntent);
         }
-
-        /*BottomNavigationBar bottomNavigationBar = (BottomNavigationBar)findViewById(R.id.bottom_navigation_bar);//底部导航栏，fragment 不提供findViewById
-        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);//MODE_SHIFTING
-        bottomNavigationBar
-                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC//STYLE_RIPPLE
-                );
-
-        bottomNavigationBar
-                .addItem(new BottomNavigationItem(R.drawable.conf, "发现").setActiveColorResource(R.color.material_orange_A200))
-                .addItem(new BottomNavigationItem(R.drawable.topic, "话题").setActiveColorResource(R.color.material_green_500))
-                .addItem(new BottomNavigationItem(R.drawable.newcon, "会议").setActiveColorResource(R.color.material_blue_A200))
-                .addItem(new BottomNavigationItem(R.drawable.friend, "好友").setActiveColorResource(R.color.material_brown_500))
-                .addItem(new BottomNavigationItem(R.drawable.mine, "我的信息").setActiveColorResource(R.color.material_blue_grey_500))
-                .setFirstSelectedPosition(0)
-                .initialise();
-        setDefaultFrag();
-
-        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
-            @Override
-            public void onTabSelected(int position) {
-                hideAllFrag();//先隐藏所有frag
-                switch (position) {
-                    case 0:
-                        if (confFrag == null) {
-                            confFrag = new ConfFragment();
-                        }
-                        addFrag(confFrag);
-                        getSupportFragmentManager().beginTransaction().show(confFrag).commit();
-                        getSupportActionBar().setTitle("会议推荐");
-                        break;
-                    case 3:
-                        if (buddyListFragment == null) {
-                            buddyListFragment = new BuddyListFragment();
-                        }
-                        addFrag(buddyListFragment);
-                        getSupportFragmentManager().beginTransaction().show(buddyListFragment).commit();
-                        getSupportActionBar().setTitle("好友");
-                        break;
-                }
-             }
-             @Override
-             public void onTabUnselected(int position) {
-             }
-             @Override
-             public void onTabReselected(int position) {
-             }
-        });*/
-
     }
 }
