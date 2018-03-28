3c09b4cf2113013956209dbb0ad58b32bacada09
lkc1997 <kechenglu97@gmail.com>
2018-03-28 11:52:57 +0800

    从服务器获取关注（参加）的会议列表

--- app/src/main/java/com/lkc97/easymeeting/ui/common/ConfListActivity.java
+++ app/src/main/java/com/lkc97/easymeeting/ui/common/ConfListActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.FindCallback;
 import com.lkc97.easymeeting.R;
 import com.lkc97.easymeeting.ui.adapter.ConfListAdapter;
 import com.lkc97.easymeeting.ui.adapter.ConfListBean;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -27,8 +34,6 @@ public class ConfListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_conf_list);
         innitView();
         showData();
-        adapter=new ConfListAdapter(dataList);
-        conf_list_recv.setAdapter(adapter);
         conf_list_recv.addItemDecoration(new MyDecoration(this, MyDecoration.VERTICAL_LIST,R.drawable.divider));
     }
 
@@ -39,7 +44,7 @@ public class ConfListActivity extends AppCompatActivity {
     }
 
     private void showData() {
-        ConfListBean[] conflistBean ={
+        /*ConfListBean[] conflistBean ={
                 new ConfListBean("会议1","已参加"),
                 new ConfListBean("会议2","待审核"),
                 new ConfListBean("会议3","过期会议"),
@@ -47,11 +52,43 @@ public class ConfListActivity extends AppCompatActivity {
                 new ConfListBean("会议5","已签到"),
                 new ConfListBean("会议6","3天后")
         };
-
         for (int i=0;i<50;i++){
             Random random = new Random();
             int index = random.nextInt(conflistBean.length);
             dataList.add(conflistBean[index]);
-        }
+        }*/
+        loadFollowedConference();
+
+    }
+    private void loadFollowedConference(){
+        final Date data=new Date();
+        AVQuery<AVObject> query = new AVQuery<>("FollowedConference");
+        query.whereEqualTo("follower", AVUser.getCurrentUser());
+        query.include("conference");
+        query.orderByDescending("createdAt");
+        query.findInBackground(new FindCallback<AVObject>() {
+            @Override
+            public void done(List<AVObject> list, AVException e) {
+                int interval;
+                AVObject conference=null;
+                String state;
+                //Log.d("Easymeeting",""+list.get(0).getAVObject("conference").getString("confName"));
+                for(AVObject followedConference:list){
+                    conference=followedConference.getAVObject("conference");
+                    //Log.d("Easymeeting",""+conference.getDate("createdAt"));
+                    interval=data.compareTo(conference.getDate("createdAt"));
+                    if(interval>0)
+                        state=interval+"天后";
+                    else if(interval==0)
+                        state="今日开始";
+                    else
+                        state="已过期";
+                    dataList.add(new ConfListBean(conference.getString("confName"),state));
+                }
+                adapter=new ConfListAdapter(dataList);
+                conf_list_recv.setAdapter(adapter);
+
+            }
+        });
     }
 }
