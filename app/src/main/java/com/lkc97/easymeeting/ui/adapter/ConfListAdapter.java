b21c4d26ebee24197c4073c6346ab6e162d2b60a
lkc1997 <kechenglu97@gmail.com>
2018-03-28 22:15:53 +0800

    添加RecyclerView点击事件

--- app/src/main/java/com/lkc97/easymeeting/ui/adapter/ConfListAdapter.java
+++ app/src/main/java/com/lkc97/easymeeting/ui/adapter/ConfListAdapter.java
@@ -1,12 +1,15 @@
 package com.lkc97.easymeeting.ui.adapter;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lkc97.easymeeting.R;
 
@@ -21,15 +24,18 @@ public class ConfListAdapter extends RecyclerView.Adapter<ConfListAdapter.ConfLi
     private List<ConfListBean> dataList;
 
     static class ConfListViewHolder extends RecyclerView.ViewHolder {
+        View confListView;
         TextView confName;
         ImageView confImage;
         TextView confState;
+        Button  quitBtn;
 
         public ConfListViewHolder(View itemView) {
             super(itemView);
+            confListView=itemView;
             confName = (TextView) itemView.findViewById(R.id.conf_list_name);
             confState = (TextView) itemView.findViewById(R.id.conf_list_state);
-
+            quitBtn=(Button)itemView.findViewById(R.id.conf_list_quit_btn);
         }
     }
 
@@ -42,7 +48,15 @@ public class ConfListAdapter extends RecyclerView.Adapter<ConfListAdapter.ConfLi
         if(context == null){
             context = parent.getContext();
         }
-        return new ConfListAdapter.ConfListViewHolder(LayoutInflater.from(context).inflate(R.layout.conf_list_item, parent, false));
+        View view=LayoutInflater.from(context).inflate(R.layout.conf_list_item, parent, false);
+        final ConfListViewHolder holder=new ConfListViewHolder(view);
+        holder.quitBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(v.getContext(),"退出会议",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return holder;
     }
 
     @Override
