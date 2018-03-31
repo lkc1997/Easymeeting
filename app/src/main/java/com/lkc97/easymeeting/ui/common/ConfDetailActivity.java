531801a48c87aa3bd6ef7f5079ff9aa19332001a
lkc1997 <kechenglu97@gmail.com>
2018-03-31 15:14:50 +0800

    优化

--- app/src/main/java/com/lkc97/easymeeting/ui/common/ConfDetailActivity.java
+++ app/src/main/java/com/lkc97/easymeeting/ui/common/ConfDetailActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
+import android.widget.TextView;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
@@ -15,14 +15,15 @@ import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.DeleteCallback;
 import com.avos.avoscloud.FindCallback;
 import com.avos.avoscloud.GetCallback;
-import com.avos.avoscloud.LogUtil;
-import com.avos.avoscloud.SaveCallback;
 import com.lkc97.easymeeting.R;
 
 import java.util.List;
 
 public class ConfDetailActivity extends AppCompatActivity {
     private AVObject conference;
+    private TextView confName;
+    private TextView confPlace;
+    private TextView confTime;
     static{
 
     }
@@ -30,19 +31,25 @@ public class ConfDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conf_detail);
+        confName=(TextView)findViewById(R.id.conf_name_detail_activity);
+        confPlace=(TextView)findViewById(R.id.conf_place_detail_activity);
+        confTime=(TextView)findViewById(R.id.conf_time_detail_activity);
         /*取得来自B页面的数据，并显示到画面*/
         Bundle bundle = this.getIntent().getExtras();
-            /*获取Bundle中的数据，注意类型和key*/
+        /*获取Bundle中的数据，注意类型和key*/
         String objectId = bundle.getString("objectId");
         AVQuery<AVObject> avQuery = new AVQuery<>("Conference");
         avQuery.getInBackground(objectId, new GetCallback<AVObject>() {
             @Override
             public void done(AVObject avObject, AVException e) {
                 conference=avObject;
+                loadConfInfo();//加载会议信息
             }
         });
         //Log.d("Easymeeting",bundle.getString("objectId"));
+
     }
+    //已弃用
     public void confQuit(View v){
         new AlertDialog.Builder(this)
                 .setTitle("确认")
@@ -72,10 +79,15 @@ public class ConfDetailActivity extends AppCompatActivity {
                 .show();
     }
     public void confParticipate(View v){
-        AVObject followedCoference=new AVObject("FollowedConference");
+        AVObject followedCoference=new AVObject("Foll,owedConference");
         AVUser mine=AVUser.getCurrentUser();
         followedCoference.put("follower", mine);
         followedCoference.put("conference",conference);
         followedCoference.saveInBackground();
+    }
+    public void loadConfInfo(){
+        confName.setText(conference.getString("confName"));
+        confPlace.setText("地点:"+conference.getString("confPlace"));
+        confTime.setText("日期:"+conference.getString("date"));
     }
 }
